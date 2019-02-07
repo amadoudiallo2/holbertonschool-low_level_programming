565c8339e56bb900c1b097d61de2ad8de214dbde
Amadou Diallo <diallo.amadousadjo@yahoo.fr>
2019-02-07 01:43:08 +0000

    c repository

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: (0 for Success)
+ *
+ */
+int main(void)
+{
+  puts("\"programming is like building a multilingual puzzle");
+  return (0);
+}
